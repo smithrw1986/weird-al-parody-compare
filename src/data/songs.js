201d4e037523e316aefa9f328b2f5f6bfbaf0e80
smithrw1986 <smithrw1986@gmail.com>
2025-07-08 17:04:45 -0500

    Add Weird Al parody-original song pairs with accurate info

---
+++ src/data/songs.js
@@ -0,0 +1,344 @@
+export const songPairs = [
+  {
+    parody: {
+      title: "Weird Al Song",
+      album: "Dare to Be Stupid",
+      year: 1985,
+      songName: "Like a Surgeon",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/7p0qZ2VNf6NfpRCMnSeVYG",
+    },
+    original: {
+      title: "Original Song",
+      album: "Like a Virgin — Madonna",
+      year: 1984,
+      songName: "Like a Virgin",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/4pYw6GhRtP7g0qMSu7m0Ec",
+    },
+  },
+  {
+    parody: {
+      title: "Eat It",
+      album: "Weird Al Yankovic in 3-D",
+      year: 1984,
+      songName: "Eat It",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/6b3EIJYyeG0uQHxNH8AkbH",
+    },
+    original: {
+      title: "Beat It",
+      album: "Thriller — Michael Jackson",
+      year: 1982,
+      songName: "Beat It",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/6epn3r7S14KUqlReYr77hA",
+    },
+  },
+  {
+    parody: {
+      title: "Amish Paradise",
+      album: "Bad Hair Day",
+      year: 1996,
+      songName: "Amish Paradise",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/38iqn5x9C5O5N9pFJ2t7AJ",
+    },
+    original: {
+      title: "Gangsta's Paradise",
+      album: "Gangsta's Paradise — Coolio",
+      year: 1995,
+      songName: "Gangsta's Paradise",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/4AfF0oRAiGeZ5r7CYFfD7I",
+    },
+  },
+  {
+    parody: {
+      title: "White & Nerdy",
+      album: "Straight Outta Lynwood",
+      year: 2006,
+      songName: "White & Nerdy",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/2Rh6i2fJCvWb9KrrqGq7gU",
+    },
+    original: {
+      title: "Ridin'",
+      album: "Sound of Revenge — Chamillionaire",
+      year: 2005,
+      songName: "Ridin'",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3n3Ppam7vgaVa1iaRUc9Lp",
+    },
+  },
+  {
+    parody: {
+      title: "Smells Like Nirvana",
+      album: "Off the Deep End",
+      year: 1992,
+      songName: "Smells Like Nirvana",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/1E51R57n4XZ9lM7AaxgqfB",
+    },
+    original: {
+      title: "Smells Like Teen Spirit",
+      album: "Nevermind — Nirvana",
+      year: 1991,
+      songName: "Smells Like Teen Spirit",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/5ghIJDpPoe3CfHMGu71E6T",
+    },
+  },
+  {
+    parody: {
+      title: "Fat",
+      album: "Even Worse",
+      year: 1988,
+      songName: "Fat",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/1wZbXpR1CyMfCVxYvBy06H",
+    },
+    original: {
+      title: "Bad",
+      album: "Bad — Michael Jackson",
+      year: 1987,
+      songName: "Bad",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3S0OXQeoh0w6AY8WQVckRW",
+    },
+  },
+  {
+    parody: {
+      title: "Bedrock Anthem",
+      album: "Alapalooza",
+      year: 1993,
+      songName: "Bedrock Anthem",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/7vRDRCcIh0b07XHtcwPa5w",
+    },
+    original: {
+      title: "Under the Bridge / Give It Away",
+      album: "Blood Sugar Sex Magik — Red Hot Chili Peppers",
+      year: 1991,
+      songName: "Under the Bridge",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3d9DChrdc6BOeFsbrZ3Is0",
+    },
+  },
+  {
+    parody: {
+      title: "Canadian Idiot",
+      album: "Straight Outta Lynwood",
+      year: 2006,
+      songName: "Canadian Idiot",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/2LDumJ3jlAXe4lhkFJFQdc",
+    },
+    original: {
+      title: "American Idiot",
+      album: "American Idiot — Green Day",
+      year: 2004,
+      songName: "American Idiot",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/6nTiIhLmQ3FWhvrGafw2zj",
+    },
+  },
+  {
+    parody: {
+      title: "I Love Rocky Road",
+      album: "Weird Al Yankovic",
+      year: 1983,
+      songName: "I Love Rocky Road",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/7LONNBoYzD6syjRzCQXDpU",
+    },
+    original: {
+      title: "I Love Rock 'n Roll",
+      album: "I Love Rock 'n Roll — Joan Jett & the Blackhearts",
+      year: 1981,
+      songName: "I Love Rock 'n Roll",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/4tCtwWceOPWzenK2HAIJSb",
+    },
+  },
+  {
+    parody: {
+      title: "Another One Rides the Bus",
+      album: "Weird Al Yankovic",
+      year: 1983,
+      songName: "Another One Rides the Bus",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/4Y9hMBDRrrEnWoVHLZcizx",
+    },
+    original: {
+      title: "Another One Bites the Dust",
+      album: "The Game — Queen",
+      year: 1980,
+      songName: "Another One Bites the Dust",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/57JVGBtBLCfHw2muk5416J",
+    },
+  },
+  {
+    parody: {
+      title: "Perform This Way",
+      album: "Alpocalypse",
+      year: 2011,
+      songName: "Perform This Way",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/7b8zYRnZXUjhQdbZlzAIXk",
+    },
+    original: {
+      title: "Born This Way",
+      album: "Born This Way — Lady Gaga",
+      year: 2011,
+      songName: "Born This Way",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/6habFhsOp2NvshLv26DqMb",
+    },
+  },
+  {
+    parody: {
+      title: "Party in the CIA",
+      album: "Alpocalypse",
+      year: 2011,
+      songName: "Party in the CIA",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3TfXh2UvepL9en5ZaY1f3T",
+    },
+    original: {
+      title: "Party in the USA",
+      album: "The Time of Our Lives — Miley Cyrus",
+      year: 2009,
+      songName: "Party in the USA",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/4zykZATq0l0bM8gBd4iJ3M",
+    },
+  },
+  {
+    parody: {
+      title: "Tacky",
+      album: "Mandatory Fun",
+      year: 2014,
+      songName: "Tacky",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3opCdKqbk8S2gE9UcRgl7z",
+    },
+    original: {
+      title: "Happy",
+      album: "G I R L — Pharrell Williams",
+      year: 2013,
+      songName: "Happy",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/60nZcImufyMA1MKQY3dcCH",
+    },
+  },
+  {
+    parody: {
+      title: "Handy",
+      album: "Mandatory Fun",
+      year: 2014,
+      songName: "Handy",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3fXkL5BPdZiZsaAJ2bXqVU",
+    },
+    original: {
+      title: "Fancy",
+      album: "The New Classic — Iggy Azalea",
+      year: 2014,
+      songName: "Fancy",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/2nLtzopw4rPReszdYBJU6h",
+    },
+  },
+  {
+    parody: {
+      title: "Word Crimes",
+      album: "Mandatory Fun",
+      year: 2014,
+      songName: "Word Crimes",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/2DkGzZtG8RK7IjDL2mDRSG",
+    },
+    original: {
+      title: "Blurred Lines",
+      album: "Blurred Lines — Robin Thicke",
+      year: 2013,
+      songName: "Blurred Lines",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/7g8h6rFxkMfFz6PjL1U2bT",
+    },
+  },
+  {
+    parody: {
+      title: "Foil",
+      album: "Mandatory Fun",
+      year: 2014,
+      songName: "Foil",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3NczFMMz9Yx8RJWb1x1o1t",
+    },
+    original: {
+      title: "Royals",
+      album: "Pure Heroine — Lorde",
+      year: 2013,
+      songName: "Royals",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/5nPOM0tE6QR2MfmA3tZIeW",
+    },
+  },
+  {
+    parody: {
+      title: "Gump",
+      album: "Bad Hair Day",
+      year: 1996,
+      songName: "Gump",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/64PpUtNdzYdS4x2maFGk3j",
+    },
+    original: {
+      title: "Lump",
+      album: "Presidents of the United States of America",
+      year: 1995,
+      songName: "Lump",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3AIBS2m8bGLbRoYpZFW6kU",
+    },
+  },
+  {
+    parody: {
+      title: "Jurassic Park",
+      album: "Alapalooza",
+      year: 1993,
+      songName: "Jurassic Park",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3ChLvNHzmQfL3GiSWAdZMi",
+    },
+    original: {
+      title: "MacArthur Park",
+      album: "The Richard Harris Love Album",
+      year: 1968,
+      songName: "MacArthur Park",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/3kCwyOf4nHCqB6Sm0GO6zk",
+    },
+  },
+  {
+    parody: {
+      title: "Pretty Fly for a Rabbi",
+      album: "Running with Scissors",
+      year: 1999,
+      songName: "Pretty Fly for a Rabbi",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/0TZ2Xj7JpUUNxeU9zBvNqH",
+    },
+    original: {
+      title: "Pretty Fly (for a White Guy)",
+      album: "Americana — The Offspring",
+      year: 1998,
+      songName: "Pretty Fly (for a White Guy)",
+      spotifyEmbedUrl:
+        "https://open.spotify.com/embed/track/4hlfTsT6RSlLz6wxyJX2vE",
+    },
+  },
+];
